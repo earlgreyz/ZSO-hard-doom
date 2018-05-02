4a39abb103ec88fc6a5d5732c381022898e4bedb
Mikołaj Walczak <mikiwalczak+github@gmail.com>
2018-05-02 20:33:08 +0200

    Load doomcode during probe

--- src/doompci.c
+++ src/doompci.c
@@ -2,6 +2,30 @@
 
 #include "doompci.h"
 #include "doompci/device.h"
+#include "doompci/doomcode.h"
+#include "doompci/registers.h"
+
+static int load_microcode(struct pci_dev *dev) {
+  void __iomem *BAR0 = pci_iomap(dev, 0, 0);
+  if (IS_ERR(BAR0)) {
+    return PTR_ERR(BAR0);
+  }
+
+  iowrite32(BAR0 + DOOMPCI_FE_CODE_ADDR, 0);
+
+  for (size_t i = 0; i < ARRAY_SIZE(doomcode); ++i) {
+    iowrite32(BAR0 + DOOMPCI_FE_CODE_WINDOW, doomcode[i]);
+  }
+
+  iowrite32(BAR0 + DOOMPCI_RESET, RESET_ALL);
+  // Initialize command iomem here (CMD_*_PTR)
+  iowrite32(BAR0 + DOOMPCI_INTR, INTR_ALL);
+  // Enable used interrupts (INTR_ENABLE) here
+  iowrite32(BAR0 + DOOMPCI_ENABLE, ENABLE_ALL & ~ENABLE_FETCH)
+
+  pci_iounmap(dev, BAR0);
+  return 0;
+}
 
 static int probe(struct pci_dev *dev, const struct pci_device_id *id) {
   int err;
@@ -19,10 +43,16 @@ static int probe(struct pci_dev *dev, const struct pci_device_id *id) {
     goto probe_request_err;
   }
 
+  err = load_microcode(dev);
+  if (IS_ERR(err)) {
+    printk(KERN_ERR "Probe error: load_microcode\n");
+    goto probe_microcode_err;
+  }
+
   printk(KERN_INFO "Probe finished\n");
   return 0;
 
-probe_iomap_err:
+probe_microcode_err:
   pci_release_regions(dev);
 probe_request_err:
   pci_disable_device(dev);
