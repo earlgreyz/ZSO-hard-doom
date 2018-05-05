a9bdf7c612a9a8e1829fa3a3feea9c667d9ce919
Mikołaj Walczak <mikiwalczak+github@gmail.com>
2018-05-05 12:10:36 +0200

    Surface ioctl functions

--- src/surface.c
+++ src/surface.c
@@ -6,10 +6,49 @@
 #include <linux/vmalloc.h>
 
 #include "surface.h"
+#include "../include/doomdev.h"
 
+static long doomsurf_copy_rects(struct file *file, struct doomdev_surf_ioctl_copy_rects *arg) {
+  return -ENOTTY;
+}
+
+static long doomsurf_fill_rects(struct file *file, struct doomdev_surf_ioctl_fill_rects *arg) {
+  return -ENOTTY;
+}
+
+static long doomsurf_draw_lines(struct file *file, struct doomdev_surf_ioctl_draw_lines *arg) {
+  return -ENOTTY;
+}
+
+static long doomsurf_draw_background(struct file *file, struct doomdev_surf_ioctl_draw_background *arg) {
+  return -ENOTTY;
+}
+
+static long doomsurf_draw_columns(struct file *file, struct doomdev_surf_ioctl_draw_columns *arg) {
+  return -ENOTTY;
+}
+
+static long doomsurf_draw_spans(struct file *file, struct doomdev_surf_ioctl_draw_spans *arg) {
+  return -ENOTTY;
+}
 
 static long doomsurf_ioctl(struct file *file, unsigned int cmd, unsigned long arg) {
-  return -ENOTTY;
+  switch (cmd) {
+    case DOOMDEV_SURF_IOCTL_COPY_RECTS:
+      return doomsurf_copy_rects(file, (struct doomdev_surf_ioctl_copy_rects *) arg);
+    case DOOMDEV_SURF_IOCTL_FILL_RECTS:
+      return doomsurf_fill_rects(file, (struct doomdev_surf_ioctl_fill_rects *) arg);
+    case DOOMDEV_SURF_IOCTL_DRAW_LINES:
+      return doomsurf_draw_lines(file, (struct doomdev_surf_ioctl_draw_lines *) arg);
+    case DOOMDEV_SURF_IOCTL_DRAW_BACKGROUND:
+      return doomsurf_draw_background(file, (struct doomdev_surf_ioctl_draw_background *) arg);
+    case DOOMDEV_SURF_IOCTL_DRAW_COLUMNS:
+      return doomsurf_draw_columns(file, (struct doomdev_surf_ioctl_draw_columns *) arg);
+    case DOOMDEV_SURF_IOCTL_DRAW_SPANS:
+      return doomsurf_draw_spans(file, (struct doomdev_surf_ioctl_draw_spans *) arg);
+    default:
+      return -ENOTTY;
+  }
 }
 
 static loff_t doomsurf_llseek(struct file *file, loff_t filepos, int whence) {
@@ -35,7 +74,7 @@ static struct file_operations surface_ops = {
 };
 
 long doomsurf_create(uint16_t width, uint16_t height) {
-  unsigned int err;
+  unsigned long err;
   int fd;
   struct file *file;
   void *surface;
@@ -46,9 +85,9 @@ long doomsurf_create(uint16_t width, uint16_t height) {
     goto create_vmalloc_err;
   }
 
-  fd = anon_inode_getfd("surface", &surface_ops, surface, O_RDONLY | O_CLOEXEC);
-  if (IS_ERR_VALUE(fd)) {
-    err = fd;
+  fd = anon_inode_getfd(SURF_FILE_TYPE, &surface_ops, surface, O_RDONLY | O_CLOEXEC);
+  if (IS_ERR_VALUE((unsigned long) fd)) {
+    err = (unsigned long) fd;
     goto create_getfd_err;
   }
 
@@ -63,7 +102,7 @@ long doomsurf_create(uint16_t width, uint16_t height) {
   return fd;
 
 create_getfile_err:
-  close(fd);
+  // close(fd); // TODO: should we close it here or not?
 create_getfd_err:
   vfree(surface);
 create_vmalloc_err:
