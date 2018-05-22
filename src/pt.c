3699705c209cd030ff60da045cf166b7d701d8ac
Mikołaj Walczak <mikiwalczak+github@gmail.com>
2018-05-22 12:31:50 +0200

    Refactor PT

--- src/pt.c
+++ src/pt.c
@@ -2,9 +2,10 @@
 
 #include <linux/errno.h>
 
-#define MK_PT_ENTRY(addr) (((addr) & 0xfffff000) | 0x00000001)
+#define PT_MAX_ENTRIES      1024
+#define PT_MK_ENTRY(addr) (((addr) & 0xfffff000) | 0x00000001)
 
-long pt_length(uint32_t buffer_size) {
+int pt_length(uint32_t buffer_size) {
   int len = buffer_size / DOOM_PAGE_SIZE;
   if (buffer_size % DOOM_PAGE_SIZE > 0) {
     ++len;
@@ -19,7 +20,8 @@ long pt_length(uint32_t buffer_size) {
 
 void pt_fill(dma_addr_t buffer, struct pt_entry *pt, int n) {
   size_t i;
+  uint32_t *pt_val = (uint32_t *)pt;
   for (i = 0; i < n; ++i) {
-    *(uint32_t *)(pt + i) = MK_PT_ENTRY(buffer + i * DOOM_PAGE_SIZE);
+    pt_val[i] = PT_MK_ENTRY(buffer + i * DOOM_PAGE_SIZE);
   }
 }
--- src/pt.h
+++ src/pt.h
@@ -4,19 +4,25 @@
 #include <linux/kernel.h>
 
 #define DOOM_PAGE_SIZE  (4 << 10)
-#define PT_MAX_ENTRIES      1024
 #define PT_ALIGNMENT     (1 << 6)
 
+/// Represents an entry in a page table.
 struct pt_entry {
   int32_t valid :  1;
   int32_t       : 11;
   int32_t addr  : 20;
 };
 
-// Number of pages required to fit a buffer of a given size.
-long pt_length(uint32_t buffer_size);
+/**
+ * Calculates the number of pages required to fit a buffer of a given size.
+ * @success returns a positive number of pages.
+ * @failure may return -EOVERFLOW if the number of pages exceeds the limit.
+ **/
+int pt_length(uint32_t buffer_size);
 
-// Fills a page table for a given buffer.
+/**
+ * Fills a page table with consecutive DMA addresses of buffer pages.
+ **/
 void pt_fill(dma_addr_t buffer, struct pt_entry *pt, int n);
 
 #endif
