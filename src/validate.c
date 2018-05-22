c5731e31729b06be0beb304a79bb31f2e58cad21
Mikołaj Walczak <mikiwalczak+github@gmail.com>
2018-05-22 16:40:38 +0200

    Fix validate function headers

--- src/validate.c
+++ src/validate.c
@@ -4,29 +4,29 @@ static bool is_on_surface(struct surface_prv *surface, uint16_t x, uint16_t y) {
   return (x <= surface->width) && (y <= surface->height);
 }
 
-bool is_valid_copy_rect(struct surface_prv *dst, struct surface_prv *src, struct doomdev_copy_rect __user *rect) {
+bool is_valid_copy_rect(struct surface_prv *dst, struct surface_prv *src, struct doomdev_copy_rect *rect) {
   return (is_on_surface(src, rect->pos_src_x, rect->pos_src_y)
       && is_on_surface(src, rect->pos_src_x + rect->width, rect->pos_src_y + rect->height)
       && is_on_surface(dst, rect->pos_dst_x, rect->pos_dst_y)
       && is_on_surface(dst, rect->pos_dst_x + rect->width, rect->pos_dst_y + rect->height));
 }
 
-bool is_valid_fill_rect(struct surface_prv *dst, struct doomdev_fill_rect __user *rect) {
+bool is_valid_fill_rect(struct surface_prv *dst, struct doomdev_fill_rect *rect) {
   return (is_on_surface(dst, rect->pos_dst_x, rect->pos_dst_y)
       && is_on_surface(dst, rect->pos_dst_x + rect->width, rect->pos_dst_y + rect->height));
 }
 
-bool is_valid_draw_line(struct surface_prv *dst, struct doomdev_line __user *line) {
+bool is_valid_draw_line(struct surface_prv *dst, struct doomdev_line *line) {
   return (is_on_surface(dst, line->pos_a_x, line->pos_a_y)
       && is_on_surface(dst, line->pos_b_x, line->pos_b_y));
 }
 
-bool is_valid_draw_column(struct surface_prv *dst, struct doomdev_column __user *column) {
+bool is_valid_draw_column(struct surface_prv *dst, struct doomdev_column *column) {
   return (is_on_surface(dst, column->x, column->y1)
       && is_on_surface(dst, column->x, column->y2));
 }
 
-bool is_valid_draw_span(struct surface_prv *dst, struct doomdev_span __user *span) {
+bool is_valid_draw_span(struct surface_prv *dst, struct doomdev_span *span) {
   return (is_on_surface(dst, span->x1, span->y)
       && is_on_surface(dst, span->x2, span->y));
 }
--- src/validate.h
+++ src/validate.h
@@ -13,26 +13,26 @@
 /**
  * Validates if a given rect is a proper rect for the dst and src surfaces.
  **/
-bool is_valid_copy_rect(struct surface_prv *dst, struct surface_prv *src, struct doomdev_copy_rect __user *rect);
+bool is_valid_copy_rect(struct surface_prv *dst, struct surface_prv *src, struct doomdev_copy_rect *rect);
 
 /**
  * Validates if a given rect is a proper rect for the dst surface.
  **/
-bool is_valid_fill_rect(struct surface_prv *dst, struct doomdev_fill_rect __user *rect);
+bool is_valid_fill_rect(struct surface_prv *dst, struct doomdev_fill_rect *rect);
 
 /**
  * Validates if a given line is a proper line for the dst surface.
  **/
-bool is_valid_draw_line(struct surface_prv *dst, struct doomdev_line __user *line);
+bool is_valid_draw_line(struct surface_prv *dst, struct doomdev_line *line);
 
 /**
  * Validates if a given columns is a proper column for the dst surface.
  **/
-bool is_valid_draw_column(struct surface_prv *dst, struct doomdev_column __user *column);
+bool is_valid_draw_column(struct surface_prv *dst, struct doomdev_column *column);
 
 /**
  * Validates if a given span is a proper span for the dst surface.
  **/
-bool is_valid_draw_span(struct surface_prv *dst, struct doomdev_span __user *span);
+bool is_valid_draw_span(struct surface_prv *dst, struct doomdev_span *span);
 
 #endif
